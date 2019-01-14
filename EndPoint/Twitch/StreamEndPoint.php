0ba3c88762801df6cfdfa8c92d8875591762ddc8
Pierre LEJEUNE <darkanakin41@gmail.com>
2019-01-14 19:38:30 +0100

    Création de la méthode permettant de récupérer les informations d'une liste de streams

--- EndPoint/Twitch/StreamEndPoint.php
+++ EndPoint/Twitch/StreamEndPoint.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace PLejeune\ApiBundle\EndPoint\Twitch;
 
 use PLejeune\ApiBundle\EndPoint\AbstractEndPoint;
@@ -16,7 +17,7 @@ class StreamEndPoint extends AbstractEndPoint
         $url_parameters = [
             "game_id" => $gameId,
         ];
-        if($cursor !== null){
+        if ($cursor !== null) {
             $url_parameters["after"] = $cursor;
         }
         $query = http_build_query($url_parameters);
@@ -25,17 +26,25 @@ class StreamEndPoint extends AbstractEndPoint
 
         return json_decode($result, true);
     }
+
     /**
      * Retrieve streams from the given user_login
+     * @param array $user_logins
+     * @param string $cursor the page
      * @return array
      */
-    public function getStream($userLogin)
+    public function getStreams(array $user_logins, $cursor = null)
     {
-        $url_parameters = [
-            "user_login" => $userLogin,
-        ];
+        $data = [];
+        foreach ($user_logins as $login) {
+            $data[] = sprintf("user_login=%s", $login);
+        }
 
-        $query = http_build_query($url_parameters);
+        if ($cursor !== null) {
+            $data[] = sprintf('after=%s', $cursor);
+        }
+
+        $query = implode("&", $data);
 
         $result = $this->getClient()->execute(self::URL . "helix/streams?" . $query);
 
